ed0eb60e5fd137d1acd3ae7bb3d9a7668fb160ac
jfouret <julien@fouret.me>
2024-07-01 17:08:05 +0200

    add S3 gateway

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -70,3 +70,17 @@ resource "aws_security_group" "security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.vpc.id
+  service_name = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [
+    aws_route_table.public.id
+  ]
+
+  tags = {
+    Name = "${var.name}-s3-endpoint"
+  }
+}
