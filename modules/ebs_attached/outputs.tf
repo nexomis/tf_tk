2a8b803ea250c6a110b70342a048027bb912a2d4
jfouret <julien@fouret.me>
2024-09-03 10:27:26 +0200

    remove snap id from outputs ebs_attached

--- modules/ebs_attached/outputs.tf
+++ modules/ebs_attached/outputs.tf
@@ -7,8 +7,3 @@ output "volume_attachment_id" {
   description = "The ID of the EBS volume attachment"
   value       = aws_volume_attachment.attach.id
 }
-
-output "dlm_lifecycle_policy_id" {
-  description = "The ID of the DLM lifecycle policy"
-  value       = aws_dlm_lifecycle_policy.snap.id
-}
